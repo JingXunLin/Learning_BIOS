c73ddfb394c79ce8f04e66f39e36b062dc708595
JingXunLin <jingxun1110@gmail.com>
2026-02-15 21:16:18 +0800

    XIP - Execute In Place

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
 void main(void) {
-    while(1);
+    volatile int *leds = (volatile int *)0xf0000000;
+    int write_bit = 1;
+    while(1) {
+        volatile int i;
+        for(i=0; i<5000000; i++);   // delay 5000000 cycles
+        *leds = write_bit;                   // write into LED
+        write_bit ^= 3;             // 1、2交換
+    }
 }
